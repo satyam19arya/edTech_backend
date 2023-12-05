f229d455a73b9852ff692456748b11820218a9f7
Satyam <satyam19arya@gmail.com>
2023-12-06 01:02:28 +0530

    initial setup

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const dbConnect = require('./dbConnect');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const cloudinary = require('cloudinary').v2;
+const fileupload = require('express-fileupload');
+dotenv.config('./.env');
+const app = express();
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+app.use(express.json({ limit: "5mb" }));
+app.use(morgan('common'));
+app.use(cookieParser());
+app.use(fileupload({
+    useTempFiles: true,
+    tempFileDir: '/tmp/'
+}));
+let origin = 'http://localhost:3000';
+if(process.env.NODE_ENV === 'production') {
+    origin = process.env.CORS_ORIGIN;
+}
+app.use(cors({
+    credentials: true,
+    origin
+}));
+
+const PORT = process.env.PORT;
+dbConnect();
+
+app.get('/', (req, res) => {
+    res.send('Hello from server 😎');
+});
+
+app.listen(PORT, () => {
+    console.log(`listening on port: ${PORT}`);
+});
